3bd1c7f45a60a089f3105eb4f9565c26a19bee40
Ceciany <120055945+Ceciany@users.noreply.github.com>
2024-07-31 00:16:42 +0000

    ok

--- test_example.py
+++ test_example.py
@@ -3,10 +3,10 @@ from menu import Calculator1
 
 @pytest.fixture
 def calc():
-    return Calculator()
+    return Calculator1()
 
-def test_adicao(calc: Calculator):
+def test_adicao(calc: Calculator1):
     assert calc.adicao(2, 3) == 5
 
-def test_subtracao(calc: Calculator):
+def test_subtracao(calc: Calculator1):
     assert calc.subtracao(5, 3) == 2
