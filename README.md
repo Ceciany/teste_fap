a99bcbb71a8d0e370185ad777d8c6c5d0dc1c74f
Ceciany <120055945+Ceciany@users.noreply.github.com>
2024-08-03 13:29:13 +0000

    ok

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # teste_fap
+
+#python -m venv venv
+#venv\Scripts\activate
+#pip install -r requeriments.txt
+
+pytest
+pytest-html
+selenium
---
+++ test_webAutomation.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys # Enviar teclas epeciais
+from selenium.webdriver.support.ui import Select # Manipular dropdown
+from selenium.webdriver.common.alert import Alert # Manipular alerts
+from selenium.webdriver.support.ui import WebDriverWait # Gerenciando esperas
+from selenium.webdriver.support import expected_conditions as EC # Gerenciando esperas
+import time
+import pytest
+
+@pytest.fixture
+def setup_teardown():
+    driver = webdriver.Chrome()
+    driver.get ("https://wcaquino.me/cypress/componentes.html")
+    yield driver
+    driver.quit
+    
+def test_botao_click_me(setup_teardown):
+    driver = setup_teardown
+    # driver = webdriver.Chrome()
+    # driver.get ("https://wcaquino.me/cypress/componentes.html")
+
+    botao = driver.find_element (By.CSS_SELECTOR, "#buttonSimple")
+
+    texto_antes = botao.get_attribute("value")
+
+    assert texto_antes == "Clique Me!"
+
+    botao.click()
+
+    assert botao.get_attribute("value") == "Obrigado!"
+
+    time.sleep(4)
+
+def test_preencer_campo_nome(setup_teardown):
+    driver = setup_teardown
+    # driver = webdriver.Chrome()
+
+    # driver.get ("https://wcaquino.me/cypress/componentes.html")
+
+    campo_nome = driver.find_element (By.CSS_SELECTOR,"#formNome")
+
+    campo_nome.send_keys("Ceciany")
+
+    time.sleep(3)
+
+    assert campo_nome.get_attribute ("value") == "Ceciany"
